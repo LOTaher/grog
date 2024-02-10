bed146a9d3d1425de392cb00a4d38b7cc832c88a
Laith Taher <lotaher04@gmail.com>
2024-02-09 21:03:12 -0500

    feat: building the http module

--- cmd/install.go
+++ cmd/install.go
@@ -4,73 +4,85 @@ import (
 	"fmt"
 	"os"
 	"strings"
+    "io/"
 
+	"github.com/LOTaher/grog/pkg/http"
 	"github.com/LOTaher/grog/pkg/util"
 	"github.com/spf13/cobra"
 )
 
 var install = &cobra.Command{
-    Use: "install",
-    Short: "Install a package.",
-    Long: `Install a package.
+	Use:   "install",
+	Short: "Install a package.",
+	Long: `Install a package.
     Example:
 
     grog install express`,
 
-    Run: installPackage,
+	Run: installPackage,
 }
 
 // Represents the package that will be installed.
 type Installer struct {
-    package_name string
-    package_version string
+	package_name    string
+	package_version string
 }
 
 // Parse the inputted package details and initializes the Installer's data.
 func (i *Installer) parsePackageDetails(pkg string) {
-    
-    parts := strings.Split(pkg, "@")
-    name := parts[0]
-    version := "latest"
 
-    if len(parts) > 1 {
-        version = parts[1]
-    }
-    
-    if version == "latest" {
-        i.package_version = version
-        i.package_name = name
-        return
-    }
+	parts := strings.Split(pkg, "@")
+	name := parts[0]
+	version := "latest"
 
+	if len(parts) > 1 {
+		version = parts[1]
+	}
 
-    ok, err := util.ValidVersion(version);
-    
-    if ok {
-        i.package_version = version
-    } else if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if version == "latest" {
+		i.package_version = version
+		i.package_name = name
+		return
+	}
 
-    i.package_name = name
+	ok, err := util.ValidVersion(version)
 
-    return
+	if ok {
+		i.package_version = version
+	} else if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	i.package_name = name
+
+	return
 }
 
 // The function that executes when the cli command is entered.
 func installPackage(cmd *cobra.Command, args []string) {
-    if len(args) < 1 {
-        fmt.Printf("Please specify a package name to install.\n")
-        return
-    }
-    
-    i := &Installer{}
+	if len(args) < 1 {
+		fmt.Printf("Please specify a package name to install.\n")
+		return
+	}
 
-    pkg := args[0]
+	i := &Installer{}
 
-    i.parsePackageDetails(pkg)
-    
-    fmt.Printf("Installing package: %s, with version: %s\n", i.package_name, i.package_version)
+	pkg := args[0]
+
+	i.parsePackageDetails(pkg)
+
+	request := http.CreateRequest(i.package_name, i.package_version)
+
+	response, err := request.Send()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+    // TODO
+
+
+	fmt.Printf("Installing package: %s, with version: %s\n", i.package_name, i.package_version)
 
 }
