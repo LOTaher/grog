732cb4915efd7e54d8c17a11127ee7843739cdd7
Laith Taher <lotaher04@gmail.com>
2024-02-15 18:06:06 -0800

    chore: organizing packages

--- cmd/install.go
+++ cmd/install.go
@@ -2,87 +2,94 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
-    "io/"
 
-	"github.com/LOTaher/grog/pkg/http"
 	"github.com/LOTaher/grog/pkg/util"
 	"github.com/spf13/cobra"
 )
 
+var npmRegistryURL = "https://registry.npmjs.org"
+
 var install = &cobra.Command{
-	Use:   "install",
-	Short: "Install a package.",
-	Long: `Install a package.
-    Example:
-
-    grog install express`,
-
-	Run: installPackage,
+    Use:   "install [package]",
+    Short: "Install a package.",
+    Long:  `Install a package. Example: grog install express`,
+    Run:   installPackage,
 }
 
-// Represents the package that will be installed.
 type Installer struct {
-	package_name    string
-	package_version string
+    Name    string
+    Version string
 }
 
-// Parse the inputted package details and initializes the Installer's data.
-func (i *Installer) parsePackageDetails(pkg string) {
+func (i *Installer) parsePackageDetails(pkg string) error {
+    parts := strings.Split(pkg, "@")
+    i.Name = parts[0]
+    i.Version = "latest" 
 
-	parts := strings.Split(pkg, "@")
-	name := parts[0]
-	version := "latest"
-
-	if len(parts) > 1 {
-		version = parts[1]
-	}
-
-	if version == "latest" {
-		i.package_version = version
-		i.package_name = name
-		return
-	}
-
-	ok, err := util.ValidVersion(version)
-
-	if ok {
-		i.package_version = version
-	} else if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	i.package_name = name
-
-	return
+    if len(parts) > 1 && parts[1] != "" {
+        ok, err := util.ValidVersion(parts[1])
+        if err != nil {
+            return fmt.Errorf("invalid version '%s': %w", parts[1], err)
+        }
+        if ok {
+            i.Version = parts[1]
+        }
+    }
+    return nil
 }
 
-// The function that executes when the cli command is entered.
 func installPackage(cmd *cobra.Command, args []string) {
-	if len(args) < 1 {
-		fmt.Printf("Please specify a package name to install.\n")
-		return
-	}
+    if len(args) < 1 {
+        fmt.Println("Please specify a package name to install.")
+        return
+    }
 
-	i := &Installer{}
+    installer := Installer{}
+    if err := installer.parsePackageDetails(args[0]); err != nil {
+        fmt.Println("Error parsing package details:", err)
+        os.Exit(1)
+    }
 
-	pkg := args[0]
+    fmt.Printf("Preparing to install package: %s@%s\n", installer.Name, installer.Version)
 
-	i.parsePackageDetails(pkg)
+    if err := performInstallation(installer.Name, installer.Version); err != nil {
+        fmt.Println("Installation failed:", err)
+        os.Exit(1)
+    }
 
-	request := http.CreateRequest(i.package_name, i.package_version)
+    fmt.Printf("Successfully installed package: %s@%s\n", installer.Name, installer.Version)
+}
 
-	response, err := request.Send()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+func performInstallation(name, version string) error {
+    url := fmt.Sprintf("%s/%s/%s", npmRegistryURL, name, version)
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        return err
+    }
 
-    // TODO
+    req.Header.Set("Accept", "application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*")
 
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
 
-	fmt.Printf("Installing package: %s, with version: %s\n", i.package_name, i.package_version)
+    // TODO: Handle package response. Only printing right now.
 
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        return err
+    }
+
+    fmt.Printf("Response from npm registry: %s\n", string(body))
+
+    // TODO: Implement actual package download and installation logic here.
+
+    return nil
 }
--- pkg/util/version.go
+++ pkg/util/version.go
@@ -6,12 +6,11 @@ import (
 	"github.com/Masterminds/semver/v3"
 )
 
-// Checks to see if a package's version is semver standards.
 func ValidVersion(version string) (bool, error) {
 
     _, err := semver.NewVersion(version)
     if err != nil {
-        return false, fmt.Errorf("Invalid version '%s' with error: %s\n", version, err)
+        return false, fmt.Errorf("invalid version '%s' with error: %s", version, err)
     }
 
     return true, nil
